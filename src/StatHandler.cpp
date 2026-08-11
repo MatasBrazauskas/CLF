78594de54d8c9ab76dfdeea98715e4dec6a53855
MatasBrazauskas <matasbrazauskas123456@gmail.com>
2026-08-11 18:07:26 +0300

    Fixed

--- src/StatHandler.cpp
+++ src/StatHandler.cpp
@@ -49,7 +49,7 @@ void StatHandler::analyzeLine(const std::optional<LineInfo> &lineOpt) {
             return;
         }
 
-        const auto hourDate = line.date.substr(1, secondColon - 1);
+        const auto hourDate = line.date.substr(0, secondColon);
         if (hourDate.empty()) {
             return;
         }
