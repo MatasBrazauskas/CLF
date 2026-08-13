36ce5cff40863b7d2267b98d26badeee52295f13
MatasBrazauskas <matasbrazauskas123456@gmail.com>
2026-08-13 13:09:15 +0300

    Fixes with number parsing

--- src/LineParser.cpp
+++ src/LineParser.cpp
@@ -4,9 +4,13 @@ LineInfo::LineInfo(std::string_view t_ipAddress, std::string_view t_identProtoco
     : ipAddress{t_ipAddress}, identProtocol{t_identProtocol}, userId{t_userId}, date{t_date}, clientRequest{t_clientRequest}, statusCode{t_statusCode}, byteCount{t_byteCount} {}
 
 static int parseStrViewToInt(const std::string_view str) {
+    if (str == "-") {
+        return 0;
+    }
+
     int val{};
     for (int i = 0; i < str.length(); ++i) {
-        val = val * 10 + (str.at(i) - '0');
+        val = val * 10 + (str[i] - '0');
     }
 
     return val;
